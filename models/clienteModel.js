2189dc5071a70f6f7a0826c111c3467e510ec46d
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2025-06-05 12:50:32 -0300

    atualizado nome de tabela de Cliente para clientes

--- models/clienteModel.js
+++ models/clienteModel.js
@@ -1,19 +1,19 @@
 import pool from '../db.js';
 
 export const listar = async () => {
-  const [rows] = await pool.query('SELECT * FROM Cliente');
+  const [rows] = await pool.query('SELECT * FROM clientes');
   return rows;
 };
 
 export const buscarPorId = async (id) => {
-  const [rows] = await pool.query('SELECT * FROM Cliente WHERE ID_Cliente = ?', [id]);
+  const [rows] = await pool.query('SELECT * FROM clientes WHERE ID_Cliente = ?', [id]);
   return rows[0];
 };
 
 export const adicionar = async (cliente) => {
   const { Nome, Telefone, Email, Senha, Historico_servico, Program_Fidelidade, Preferencia } = cliente;
   const [result] = await pool.query(
-    `INSERT INTO Cliente (Nome, Telefone, Email, Senha) VALUES (?, ?, ?, ?)`,
+    `INSERT INTO clientes (Nome, Telefone, Email, Senha) VALUES (?, ?, ?, ?)`,
     [Nome, Telefone, Email, Senha]
   );
   return { id: result.insertId, ...cliente };
@@ -25,7 +25,7 @@ export const editar = async (id, novosDados) => {
   const setString = keys.map(key => `${key} = ?`).join(', ');
   
   const [result] = await pool.query(
-    `UPDATE Cliente SET ${setString} WHERE ID_Cliente = ?`,
+    `UPDATE clientes SET ${setString} WHERE ID_Cliente = ?`,
     [...values, id]
   );
 
@@ -35,6 +35,6 @@ export const editar = async (id, novosDados) => {
 };
 
 export const excluir = async (id) => {
-  const [result] = await pool.query('DELETE FROM Cliente WHERE ID_Cliente = ?', [id]);
+  const [result] = await pool.query('DELETE FROM clientes WHERE ID_Cliente = ?', [id]);
   return result.affectedRows > 0;
 };
