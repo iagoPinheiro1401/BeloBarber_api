ba2fb7b1c6e841ff85a0cc24e4d595b8c18b1fbf
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2025-06-05 15:27:40 -0300

    atualizado cors

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ const app = express();
 dotenv.config();
 export const jwtSecret = process.env.JWT_SECRET;
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
-
+app.use(cors());
 
 app.use(express.json());
 app.use('/clientes', clienteRoutes);
